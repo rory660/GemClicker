85f2b8dc135ecd14637183a00773ff3bad5eba20
Rory Brown <rorybrown660@gmail.com>
2018-08-24 19:45:25 +0100

    rebalanced other upgrades to balance against pick

--- clickerScript2.js
+++ clickerScript2.js
@@ -4,8 +4,8 @@ windowWidth=window.innerWidth;
 windowHeight=window.innerHeight;
 
 itemPick=new Item("Pickaxe",10,1,"click",1,"pickaxe");
-itemDPS=[new Item("Miner",100,1,"damage",0,"miner"),new Item("Driller",10000,10,"damage",0,"driller"),new Item("Mining Robot",1000000,100,"damage",0,"robot"),new Item("Orbital Laser",100000000,1000,"damage",0,"laser")]
-itemCPS=[new Item("Crystal Growing Kit",1000,0.5,"cash",0,"grower"),new Item("Gift Shop",100000,5,"cash",0,"giftshop"),new Item("Time Machine",10000000,50,"cash",0,"timemachine")]
+itemDPS=[new Item("Miner",100,5,"damage",0,"miner"),new Item("Driller",10000,50,"damage",0,"driller"),new Item("Mining Robot",1000000,500,"damage",0,"robot"),new Item("Orbital Laser",100000000,5000,"damage",0,"laser")]
+itemCPS=[new Item("Crystal Growing Kit",1000,1,"cash",0,"grower"),new Item("Gift Shop",100000,10,"cash",0,"giftshop"),new Item("Time Machine",10000000,100,"cash",0,"timemachine")]
 var cookies = document.cookie.split(";");
 gem1 = new Gem("gem1");
 buttonLeft = new MenuButton("buttonLeft");
